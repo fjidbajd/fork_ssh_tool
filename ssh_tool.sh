2e31b4b67b9442edda7c7589e6cabe713c91ee11
eooce <142894633+eooce@users.noreply.github.com>
2024-05-15 01:58:24 +0800

    Update ssh_tool.sh

--- ssh_tool.sh
+++ ssh_tool.sh
@@ -5118,7 +5118,7 @@ EOF
       echo -e "${yellow}---------------------------------------------------------${re}"
       echo -e "${yellow}        单协议                    XRAY面板及其他${re}"
       echo -e "${yellow}---------------------------------------------------------${re}"
-      echo -e "${white} 9. 老王Hysteria2一键脚本     13.新版Xray面板一键脚本${re}"
+      echo -e "${white} 9. 老王Hysteria2一键脚本      13.新版Xray面板一键脚本${re}"
       echo -e "${white}10. M佬Juicity一键脚本        14.伊朗版Xray面板一键脚本${re}"
       echo -e "${white}11. M佬Tuic-v5一键脚本        15.OpenVPN一键安装脚本 ${re}"
       echo -e "${white}12. Brutal-Reality一键脚本    16.一键搭建TG代理 ${re}"
@@ -5252,20 +5252,76 @@ EOF
             fi
         ;;
         9)
+        while true; do
         clear
-            read -p $'\033[1;35m请输入Hysteria2节点端口(nat小鸡请输入可用端口范围内的端口),回车跳过则使用随机端口：\033[0m' port
-            [[ -z $port ]]
-            until [[ -z $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$port") ]]; do
-                if [[ -n $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$port") ]]; then
-                    echo -e $'\033[1;91m${port}端口已经被其他程序占用，请更换端口重试！\033[0m'
-                    read -p "设置 Hysteria2 端口[1-65535]（回车将使用随机端口）：" port
-                    [[ -z $HY2_PORT ]] && port=8880
-                fi
-            done
+          echo "--------------"
+          echo -e "${green}1.安装Hysteria2${re}"
+          echo -e "${red}2.卸载Hysteria2${re}"
+          echo -e "${yellow}3.更换Hysteria2端口${re}"          
+          echo "--------------"
+          echo -e "${skyblue}0. 返回上一级菜单${re}"
+          echo "--------------"
+          read -p $'\033[1;91m请输入你的选择: \033[0m' sub_choice
+            case $sub_choice in
+                1)
+                  clear
+                    read -p $'\033[1;35m请输入Hysteria2节点端口(nat小鸡请输入可用端口范围内的端口),回车跳过则使用随机端口：\033[0m' port
+                    [[ -z $port ]]
+                    until [[ -z $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$port") ]]; do
+                        if [[ -n $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$port") ]]; then
+                            echo -e $'\033[1;91m${port}端口已经被其他程序占用，请更换端口重试！\033[0m'
+                            read -p $'\033[1;35m设置Hysteria2端口[1-65535]（回车将使用随机端口）：\033[0m' port
+                            [[ -z $HY2_PORT ]] && port=8880
+                        fi
+                    done
 
-            HY2_PORT=$port bash -c "$(curl -L https://raw.githubusercontent.com/eooce/xray-reality/master/Hysteria2.sh)"
-            sleep 2
-            break_end
+                    HY2_PORT=$port bash -c "$(curl -L https://raw.githubusercontent.com/eooce/xray-reality/master/Hysteria2.sh)"
+                    sleep 2
+                    break_end
+
+                    ;;
+                2)
+                    systemctl stop hysteria-server.service
+                    rm /usr/local/bin/hysteria
+                    rm /etc/systemd/system/hysteria-server.service
+                    rm /etc/hysteria/config.yaml
+                    sudo systemctl daemon-reload
+                    clear
+                    echo -e "${green}Hysteria2已卸载${re}"
+                    break_end
+
+                    ;;
+                3)
+                    clear
+                        read -p $'\033[1;35m设置Hysteria2端口[1-65535]（回车跳过将使用随机端口）：\033[0m' new_port
+                        [[ -z $new_port ]] && new_port=$(shuf -i 2000-65000 -n 1)
+
+                        until [[ -z $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$new_port") ]]; do
+                            if [[ -n $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$new_port") ]]; then
+                                echo -e $'\033[1;91m${vport}端口已经被其他程序占用，请更换端口重试！\033[0m'
+                                read -p $'\033[1;35m设置Hysteria2端口[1-65535]（回车跳过将使用随机端口）：\033[0m' new_port
+                                [[ -z $new_port ]] && new_port=$(shuf -i 2000-65000 -n 1)
+                            fi
+                        done
+
+                        clear
+                        sed -i "s/^listen: :[0-9]*/listen: :$new_port/" /etc/hysteria/config.yaml
+                        systemctl restart hysteria-server.service
+                        echo -e "${green}Hysteria2端口已更换成$new_port,请手动更改客户端配置!${re}"
+                        sleep 1   
+                        break_end
+
+                    ;;
+
+                0)
+                    break
+
+                    ;;                   
+                *)
+                    echo -e "${red}无效的输入!${re}"
+                    ;;
+            esac  
+        done
         ;;     
         10)
         clear
@@ -5335,21 +5391,86 @@ EOF
         ;;
 
         17)
+        while true; do
         clear
-            read -p $'\033[1;35m请输入reality节点端口(nat小鸡请输入可用端口范围内的端口),回车跳过则使用随机端口：\033[0m' port
-            [[ -z $port ]]
-            until [[ -z $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$port") ]]; do
-                if [[ -n $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$port") ]]; then
-                    echo -e $'\033[1;91m${port}端口已经被其他程序占用，请更换端口重试！\033[0m'
-                    read -p "设置 reality 端口[1-65535]（回车将使用随机端口）：" port
-                    [[ -z $PORT ]] && port=$(shuf -i 2000-65000 -n 1)
-                fi
-            done
+          echo "--------------"
+          echo -e "${green}1.安装Reality${re}"
+          echo -e "${red}2.卸载Reality${re}"
+          echo -e "${yellow}3.更换Reality端口${re}"          
+          echo "--------------"
+          echo -e "${skyblue}0. 返回上一级菜单${re}"
+          echo "--------------"
+          read -p $'\033[1;91m请输入你的选择: \033[0m' sub_choice
+            case $sub_choice in
+                1)
+                  clear
+                    read -p $'\033[1;35m请输入reality节点端口(nat小鸡请输入可用端口范围内的端口),回车跳过则使用随机端口：\033[0m' port
+                    [[ -z $port ]]
+                    until [[ -z $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$port") ]]; do
+                        if [[ -n $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$port") ]]; then
+                            echo -e $'\033[1;91m${PORT}端口已经被其他程序占用，请更换端口重试！\033[0m'
+                            read -p $'\033[1;35m设置 reality 端口[1-65535]（回车跳过将使用随机端口）：\033[0m' port
+                            [[ -z $PORT ]] && port=$(shuf -i 2000-65000 -n 1)
+                        fi
+                    done
 
-            PORT=$port bash -c "$(curl -L https://raw.githubusercontent.com/eooce/xray-reality/master/reality.sh)"
-            sleep 2
-            break_end
-        ;; 
+                    PORT=$port bash -c "$(curl -L https://raw.githubusercontent.com/eooce/xray-reality/master/reality.sh)"
+                    sleep 2
+                    break_end
+
+                    ;;
+                2)
+                    sudo systemctl stop xray
+                    sudo rm /usr/local/bin/xray
+                    sudo rm /etc/systemd/system/xray.service
+                    sudo rm /usr/local/etc/xray/config.json
+                    sudo rm /usr/local/share/xray/geoip.dat
+                    sudo rm /usr/local/share/xray/geosite.dat
+                    sudo rm /etc/systemd/system/xray@.service
+
+                    # Reload the systemd daemon
+                    sudo systemctl daemon-reload
+
+                    # Remove any leftover Xray files or directories
+                    sudo rm -rf /var/log/xray /var/lib/xray
+
+                    clear
+                    echo -e "\e[1;32mReality已卸载\033[0m"
+                    break_end
+
+                    ;;
+                3)
+                    clear
+                        read -p $'\033[1;35m设置 reality 端口[1-65535]（回车跳过将使用随机端口）：\033[0m' new_port
+                        [[ -z $new_port ]] && new_port=$(shuf -i 2000-65000 -n 1)
+
+                        until [[ -z $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$new_port") ]]; do
+                            if [[ -n $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$vnew_port") ]]; then
+                                echo -e $'\033[1;91m${new_port}端口已经被其他程序占用，请更换端口重试！\033[0m'
+                                read -p $'\033[1;35m设置reality端口[1-65535]（回车跳过将使用随机端口）：\033[0m' new_port
+                                [[ -z $new_port ]] && new_port=$(shuf -i 2000-65000 -n 1)
+                            fi
+                        done
+
+                        install jq 
+                        clear
+                        jq --argjson new_port "$new_port" '.inbounds[0].port = $new_port' /usr/local/etc/xray/config.json > tmp.json && mv tmp.json /usr/local/etc/xray/config.json
+                        systemctl restart xray.service
+                        echo -e "${green}Reality端口已更换成$new_port,请手动更改客户端配置!${re}"
+                        sleep 1   
+                        break_end
+
+                    ;;
+                0)
+                    break
+
+                    ;;
+                *)
+                    echo -e "${red}无效的输入!${re}"
+                    ;;
+            esac  
+        done
+        ;;
 
         18)
         while true; do
