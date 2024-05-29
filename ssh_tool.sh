818949c92e0d04e1eef2da4ce44302cb5579a099
eooce <142894633+eooce@users.noreply.github.com>
2024-05-30 04:02:47 +0800

    Update ssh_tool.sh

--- ssh_tool.sh
+++ ssh_tool.sh
@@ -5397,8 +5397,8 @@ EOF
                   clear
                     read -p $'\033[1;35m请输入reality节点端口(nat小鸡请输入可用端口范围内的端口),回车跳过则使用随机端口：\033[0m' port
                     [[ -z $port ]]
-                    until [[ -z $(netstat -tuln | grep -w udp | awk '{print $4}' | sed 's/.*://g' | grep -w "$port") ]]; do
-                        if [[ -n $(netstat -tuln | grep -w udp | awk '{print $4}' | sed 's/.*://g' | grep -w "$port") ]]; then
+                    until [[ -z $(netstat -tuln | grep -w tcp | awk '{print $4}' | sed 's/.*://g' | grep -w "$port") ]]; do
+                        if [[ -n $(netstat -tuln | grep -w tcp | awk '{print $4}' | sed 's/.*://g' | grep -w "$port") ]]; then
                             echo -e "${red}端口已经被其他程序占用，请更换端口重试${re}"
                             read -p $'\033[1;35m设置 reality 端口[1-65535]（回车跳过将使用随机端口）：\033[0m' port
                             [[ -z $PORT ]] && port=$(shuf -i 2000-65000 -n 1)
@@ -5413,6 +5413,12 @@ EOF
                     break_end
                     ;;
                 2)
+                if [ -f "/etc/alpine-release" ]; then
+                    pkill -f '[w]eb'
+                    pkill -f '[n]pm'
+                    cd && rm -rf app
+                    clear
+                else
                     sudo systemctl stop xray
                     sudo rm /usr/local/bin/xray
                     sudo rm /etc/systemd/system/xray.service
@@ -5426,20 +5432,19 @@ EOF
 
                     # Remove any leftover Xray files or directories
                     sudo rm -rf /var/log/xray /var/lib/xray
-
                     clear
+                  fi
+
                     echo -e "\e[1;32mReality已卸载\033[0m"
                     break_end
-
                     ;;
                 3)
                     clear
                         read -p $'\033[1;35m设置 reality 端口[1-65535]（回车跳过将使用随机端口）：\033[0m' new_port
                         [[ -z $new_port ]] && new_port=$(shuf -i 2000-65000 -n 1)
-
-                        until [[ -z $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$new_port") ]]; do
-                            if [[ -n $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$vnew_port") ]]; then
-                                echo -e $'\033[1;91m${new_port}端口已经被其他程序占用，请更换端口重试！\033[0m'
+                        until [[ -z $(netstat -tuln | grep -w tcp | awk '{print $4}' | sed 's/.*://g' | grep -w "$port") ]]; do
+                            if [[ -n $(netstat -tuln | grep -w tcp | awk '{print $4}' | sed 's/.*://g' | grep -w "$port") ]]; then
+                                echo -e "${red}${new_port}端口已经被其他程序占用，请更换端口重试${re}"
                                 read -p $'\033[1;35m设置reality端口[1-65535]（回车跳过将使用随机端口）：\033[0m' new_port
                                 [[ -z $new_port ]] && new_port=$(shuf -i 2000-65000 -n 1)
                             fi
