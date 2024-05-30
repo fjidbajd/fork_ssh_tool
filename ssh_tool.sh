86281d05ded67395cea52769815231bc7f2fe2b9
eooce <142894633+eooce@users.noreply.github.com>
2024-05-30 09:48:34 +0800

    Update ssh_tool.sh

--- ssh_tool.sh
+++ ssh_tool.sh
@@ -5258,38 +5258,45 @@ EOF
                   clear
                     read -p $'\033[1;35m请输入Hysteria2节点端口(nat小鸡请输入可用端口范围内的端口),回车跳过则使用随机端口：\033[0m' port
                     [[ -z $port ]]
-                    until [[ -z $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$port") ]]; do
-                        if [[ -n $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$port") ]]; then
-                            echo -e $'\033[1;91m${port}端口已经被其他程序占用，请更换端口重试！\033[0m'
+                    until [[ -z $(netstat -tuln | grep -w udp | awk '{print $4}' | sed 's/.*://g' | grep -w "$port") ]]; do
+                        if [[ -n $(netstat -tuln | grep -w udp | awk '{print $4}' | sed 's/.*://g' | grep -w "$port") ]]; then
+                            echo -e "${red}${port}端口已经被其他程序占用，请更换端口重试${re}"
                             read -p $'\033[1;35m设置Hysteria2端口[1-65535]（回车将使用随机端口）：\033[0m' port
                             [[ -z $HY2_PORT ]] && port=8880
                         fi
                     done
-
-                    HY2_PORT=$port bash -c "$(curl -L https://raw.githubusercontent.com/eooce/scripts/master/Hysteria2.sh)"
+                    if [ -f "/etc/alpine-release" ]; then
+                        SERVER_PORT=$port bash -c "$(curl -L https://raw.githubusercontent.com/eooce/scripts/master/containers-shell/hy2.sh)"
+                    else
+                        HY2_PORT=$port bash -c "$(curl -L https://raw.githubusercontent.com/eooce/scripts/master/Hysteria2.sh)"
+                    fi
                     sleep 1
                     break_end
 
                     ;;
                 2)
-                    systemctl stop hysteria-server.service
-                    rm /usr/local/bin/hysteria
-                    rm /etc/systemd/system/hysteria-server.service
-                    rm /etc/hysteria/config.yaml
-                    sudo systemctl daemon-reload
-                    clear
+                    if [ -f "/etc/alpine-release" ]; then
+                        pkill -f '[w]eb'
+                        pkill -f '[n]pm'
+                        cd && rm -rf web npm server.crt server.key
+                    else
+                        systemctl stop hysteria-server.service
+                        rm /usr/local/bin/hysteria
+                        rm /etc/systemd/system/hysteria-server.service
+                        rm /etc/hysteria/config.yaml
+                        sudo systemctl daemon-reload
+                        clear
+                    fi
                     echo -e "${green}Hysteria2已卸载${re}"
                     break_end
-
                     ;;
                 3)
                     clear
                         read -p $'\033[1;35m设置Hysteria2端口[1-65535]（回车跳过将使用随机端口）：\033[0m' new_port
                         [[ -z $new_port ]] && new_port=$(shuf -i 2000-65000 -n 1)
-
-                        until [[ -z $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$new_port") ]]; do
-                            if [[ -n $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$new_port") ]]; then
-                                echo -e $'\033[1;91m${vport}端口已经被其他程序占用，请更换端口重试！\033[0m'
+                        until [[ -z $(netstat -tuln | grep -w udp | awk '{print $4}' | sed 's/.*://g' | grep -w "$new_port") ]]; do
+                            if [[ -n $(netstat -tuln | grep -w udp | awk '{print $4}' | sed 's/.*://g' | grep -w "$new_port") ]]; then
+                                echo -e "${red}${new_port}端口已经被其他程序占用，请更换端口重试${re}"
                                 read -p $'\033[1;35m设置Hysteria2端口[1-65535]（回车跳过将使用随机端口）：\033[0m' new_port
                                 [[ -z $new_port ]] && new_port=$(shuf -i 2000-65000 -n 1)
                             fi
@@ -5399,7 +5406,7 @@ EOF
                     [[ -z $port ]]
                     until [[ -z $(netstat -tuln | grep -w tcp | awk '{print $4}' | sed 's/.*://g' | grep -w "$port") ]]; do
                         if [[ -n $(netstat -tuln | grep -w tcp | awk '{print $4}' | sed 's/.*://g' | grep -w "$port") ]]; then
-                            echo -e "${red}端口已经被其他程序占用，请更换端口重试${re}"
+                            echo -e "${red}${port}端口已经被其他程序占用，请更换端口重试${re}"
                             read -p $'\033[1;35m设置 reality 端口[1-65535]（回车跳过将使用随机端口）：\033[0m' port
                             [[ -z $PORT ]] && port=$(shuf -i 2000-65000 -n 1)
                         fi
