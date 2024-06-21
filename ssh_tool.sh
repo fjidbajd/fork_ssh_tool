d1cf4a4fbdf4b227a3b406a5ef1d086d549c45a6
eooce <142894633+eooce@users.noreply.github.com>
2024-06-22 05:12:55 +0800

    Update ssh_tool.sh

--- ssh_tool.sh
+++ ssh_tool.sh
@@ -537,6 +537,8 @@ echo -e "${green}00. 脚本更新${red}                  88. 退出脚本${re}"
 echo -e "${yellow}-------------------------------------------------------------------${re}"
 read -p $'\033[1;91m请输入你的选择: \033[0m' choice
 
+trap 'echo "已退出脚本"; exit' INT
+
 case $choice in
   1)
     clear
@@ -6945,5 +6947,3 @@ EOF
 esac
     break_end
 done
-
-trap 'echo "已退出脚本"; exit' INT
